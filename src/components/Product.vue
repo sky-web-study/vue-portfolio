d14c9ee040f3d21fc8851a777a3276d2b07c9624
sky-web-study <topaz_11th@yahoo.co.jp>
2021-01-31 17:32:19 +0900

    vue portfolio チュートリアル

---
+++ src/components/Product.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-col cols="12" sm="6" md="4" lg="4">
+    <v-card class="mx-auto">
+    <v-img class="white--text align-end" height="200px" src="https://dummyimage.com/250x150/000/fff">
+    <v-card-title>{{ product.name }}</v-card-title>
+    </v-img>
+    <v-card-subtitle class="pb-0"></v-card-subtitle>
+    <v-card-text class="text--primary">
+      <div>{{ product.year }}</div>
+      <div>{{ product.category }}</div>
+      <div>{{ product.use }}</div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="orange" text>LINK </v-btn>
+    </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+<script>
+export default {
+  name: "Product", props: ["product"]};
+</script>
+<style></style>
